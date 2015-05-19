77c38494750b5fd5fe6a0709f3b712c5435a9cc4
Alexandre A. D. de Oliveira <alexandre@sparkcup.com>
2015-05-19 12:16:59 -0300

    Adding tests classes to learn how to use phpunit

---
+++ tests/BasicTest.php
@@ -0,0 +1,23 @@
+<?php
+ namespace TutorialTest;
+ use Silex\WebTestCase;
+ class BasicTest extends WebTestCase
+ {
+     public function createApplication()
+     {
+         $app = require __DIR__ . '/../src/app.php';
+         $app['debug'] = true;         return $app;
+     }
+
+     public function testStatusRoute()
+     {
+         $client = $this->createClient();
+         $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertEquals(
+            'Running with log',
+            $client->getResponse()->getContent()
+        );
+    }
+}
