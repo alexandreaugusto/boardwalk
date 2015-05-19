d47045e98ed5224889eac3fb0b8c93c6903b2c2f
Alexandre A. D. de Oliveira <alexandre@sparkcup.com>
2015-05-19 10:26:01 -0300

    Adding tests classes to learn how to use phpunit

--- src/app.php
+++ src/app.php
@@ -1,6 +1,34 @@
 <?php
 
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Silex\Provider\MonologServiceProvider;
+
+$app = new Silex\Application();
+
 $app['debug'] = true;
 $app['charset'] = "iso-8859-1";
+
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__ . '/../logs/dev.log'
+));
+
+$app
+    ->match('/calculator/add/{a}/{b}', 'Tutorial\Controller\Calculator::executeAdd')
+    ->method('GET|POST');
+$app
+    ->match('/calculator/add/', 'Tutorial\Controller\Calculator::executeIndex')
+    ->method('GET|POST');
+/*$app
+    ->match('/', 'Tutorial\Controller\Status::index')
+    ->method('GET|POST');*/
+
+$app
+    ->match('/', function () use ($app) {
+        $app['monolog']->addInfo('Logging example in the status route');
+
+        return 'Running with log';
+    })
+    ->method('GET|POST');
 
 return $app;
--- web/index.php
+++ web/index.php
@@ -1,11 +1,8 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+ini_set('display_errors', 1);
 
-$app = new Silex\Application();
-
-$app->get('/hello', function () {
-    return 'Hello!';
-});
+require __DIR__.'/../src/app.php';
+#require __DIR__.'/../src/controllers.php';
 
 $app->run();
